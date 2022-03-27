fa347dc4b4ec61d8b22c9a64d1168f6ff691f819
Elian Bollue <elian.bollue@student.ucll.be>
2022-03-27 16:28:47 +0200

    dingetjes

--- course-material/labs/preprocessor/string-switch/app.cpp
+++ course-material/labs/preprocessor/string-switch/app.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 // Write macros here
-
+#define SWITCH_STRING(string) auto _switch_string_ = string;
+#define STRING_CASE(case)     if ( _switch_string_ == #case )
+#define STRING_ELSE           else
 
 
 int foo(std::string tag)
